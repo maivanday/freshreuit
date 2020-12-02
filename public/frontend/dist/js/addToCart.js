07ee74f6008dc6135c7b13b491127a1adb3f52e7
= <=>
2020-12-02 20:59:30 +0700

    2/12

--- public/frontend/dist/js/addToCart.js
+++ public/frontend/dist/js/addToCart.js
@@ -76,34 +76,7 @@ function cartDelete(event) {
     })
 
 
-        //    function cartUpdate(event) {
-        //     event.preventDefault();
-        //     let urlUpdate = $('.update_cart_url').data('url');
-        //     let id = $(this).data('id');
-        //     let quantity = $(this).parents('tr').find('input').val();
 
-
-        //     $.ajax({
-        //         type: "GET",
-        //         url: urlUpdate,
-        //         data: {
-        //             id: id,
-        //             quantity: quantity
-        //         },
-        //         success: function(data) {
-
-        //             if (data.code === 200) {
-        //                 $('.cart_wrapper').html(data.cartUpdate);
-
-        //             }
-        //         },
-        //         error: function() {
-
-        //         }
-
-        //     });
-
-        //}
 
 
 //---------------------------
